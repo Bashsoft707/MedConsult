9fa1a426a6910453dbe3c83f5c95b52c6f5b5050
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-09-17 18:40:01 +0100

    delete node modules

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.svg";
@@ -22,7 +21,7 @@ const LoginForm = () => {
 
   const handleSubmit = async (email, password) => {
     try {
-      await auth.signInWithEmailAndPassword(email, password)
+      await auth.signInWithEmailAndPassword(auth, email, password)
       .then((userDetails) => {
         const userData = userDetails.user.data;
         console.log(userData)
